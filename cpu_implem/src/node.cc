2673825fa2d568d65e513363aac5bfbc9aa90775
Alexandre Girard <alexandre.girard@epita.fr>
2020-05-22 17:29:44 +0200

    [INIT] added image load header file and node class

---
+++ cpu_implem/src/node.cc
@@ -0,0 +1,10 @@
+//
+// Created by gigi on 5/22/20.
+//
+
+#include "node.hh"
+
+Node::Node() {
+    weight_snk = 0.f;
+    weight_src = 0.f
+}
---
+++ cpu_implem/src/node.hh
@@ -0,0 +1,21 @@
+//
+// Created by gigi on 5/22/20.
+//
+
+#ifndef GRAPH_CUTS_NODE_HH
+#define GRAPH_CUTS_NODE_HH
+
+
+class Node {
+public:
+    Node();
+
+    float weight_snk;
+    float weight_src;
+
+    // Neighbor;
+
+};
+
+
+#endif //GRAPH_CUTS_NODE_HH
