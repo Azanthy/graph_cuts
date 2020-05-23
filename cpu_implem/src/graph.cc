ee0e78962089fe739190f640e83f5cbb2eef15cf
Alexandre Girard <alexandre.girard@epita.fr>
2020-05-23 17:35:02 +0200

    [INIT] refacto init without node class

--- cpu_implem/src/graph.cc
+++ cpu_implem/src/graph.cc
@@ -18,27 +18,36 @@ Graph::Graph(char *img, char *seeds) {
     unsigned char *data = stbi_load(img, &this->_width, &this->_height, &n, 0);
     unsigned char *labels = stbi_load(seeds, &tmp_w, &tmp_h, &n, 0);
 
-    this->_nodes = std::vector<std::shared_ptr<Node>>();
     size_t *bck_histo = new size_t[256/HISTO_FACTOR]();
     size_t *obj_histo = new size_t[256/HISTO_FACTOR]();
+
+    this->_size = this->_width * this->_height;
+    this->_heights = std::vector<int>(this->_size, 0);
+    this->_excess_flow = std::vector<float>(this->_size,0.f);
+    this->_neighbors[0] = std::vector<float>(this->_size,0.f);
+    this->_neighbors[1] = std::vector<float>(this->_size,0.f);
+    this->_neighbors[2] = std::vector<float>(this->_size,0.f);
+    this->_neighbors[3] = std::vector<float>(this->_size,0.f);
+
+    auto grays = std::vector<int>(this->_size, 0);
 
     // Initialize nodes and histograms
     for (auto y = 0; y < this->_height; y++) {
         for (auto x = 0; x < this->_width; x++) {
-            auto idx = y * this->_width * n + x * n;
-            char gray = 0.299 * data[idx] + 0.587 * data[idx+1] + 0.114 * data[idx+2];
+            auto idx = y * this->_width + x;
+            auto n_idx = y * this->_width * n + x * n;
+            char gray = 0.299 * data[n_idx] + 0.587 * data[n_idx+1] + 0.114 * data[n_idx+2];
 
-            auto node = std::make_shared<Node>(gray);
+            grays[idx] = gray;
             // Histograms of size 256/4=64
             if (labels[idx]) {
                 bck_histo[gray/HISTO_FACTOR] += 1;
-                node->_excess_flow = -1.f;
+                this->_excess_flow[idx] = -1.f;
             }
             if (labels[idx+2]) {
                 obj_histo[gray/HISTO_FACTOR] += 1;
-                node->_excess_flow = 1.f;
+                this->_excess_flow[idx] = 1.f;
             }
-            this->_nodes.push_back(node);
         }
     }
 
@@ -52,18 +61,20 @@ Graph::Graph(char *img, char *seeds) {
     //Can be parallelize easily
     for (auto y = 0; y < this->_height; y++) {
         for (auto x = 0; x < this->_width; x++) {
-            auto node = this->_nodes[y * this->_width + x];
-            auto idx = node->_gray_val / HISTO_FACTOR;
+            auto idx = y * this->_width + x;
+            auto hist_val = grays[idx] / HISTO_FACTOR;
 
             // Initialize external capacities if not on seeds
-            if (node->_excess_flow == 0.f) {
-                float weight_snk = 1.f - std::exp(-norm_bck_histo[idx]);
-                float weight_src = 1.f - std::exp(-norm_obj_histo[idx]);
-                node->_excess_flow = weight_src - weight_snk;
+            if (this->_excess_flow[idx] == 0.f) {
+                float weight_snk = 1.f - std::exp(-norm_bck_histo[hist_val]);
+                float weight_src = 1.f - std::exp(-norm_obj_histo[hist_val]);
+                this->_excess_flow[idx] = weight_src - weight_snk;
             }
+            if (this->_excess_flow[idx] > 0.f)
+                this->_nb_active += 1;
 
             // Initialize neighbors capacities
-            initialize_node_capacities(x, y, node);
+            initialize_node_capacities(x, y, grays);
         }
     }
 
@@ -71,6 +82,7 @@ Graph::Graph(char *img, char *seeds) {
     stbi_image_free(data);
 
 }
+
 
 void Graph::normalize_histo(size_t *bck_histo, size_t *obj_histo, float **norm_bck_histo,
     float **norm_obj_histo, float *sum_bck_histo, float *sum_obj_histo) {
@@ -102,26 +114,53 @@ void Graph::normalize_histo(size_t *bck_histo, size_t *obj_histo, float **norm_b
     return;
 }
 
-void Graph::initialize_node_capacities(int x, int y, shared_node node) {
-    // Up
-    if (y > 0) {
-        auto up_node = _nodes[(y-1) * _width + x];
-        node->_capacities[0] = DISTANCE(node->_gray_val, up_node->_gray_val);
-        auto tmp = 1 - std::exp(-std::abs(node->_gray_val - up_node->_gray_val)/255.f);
+void Graph::initialize_node_capacities(int x, int y, std::vector<int> &grays) {
+    auto idx_curr = y * this->_width + x;
+    for (auto i = 0; i < 4; i++) {
+        auto idx_nghb = (y + this->y_nghb[i]) * _width + (x + this->x_nghb[i]);
+        if (y + y_nghb[i] >= 0 && y + y_nghb[i] < _height &&
+            x + x_nghb[i] >= 0 && x + x_nghb[i] < _width)
+            this->_neighbors[i][idx_curr] = DISTANCE(grays[idx_curr], grays[idx_nghb]);
     }
-    // Right
-    if (x < this->_width-1) {
-        auto right_node = _nodes[y * _width + (x+1)];
-        node->_capacities[1] = DISTANCE(node->_gray_val, right_node->_gray_val);
+}
+
+void Graph::max_flow()
+{
+    while (_nb_active) {
+        for (auto y = 0; y < this->_height; y++)
+            for (auto x = 0; x < this->_width; x++)
+                relabel(x, y);
+        for (auto y = 0; y < this->_height; y++)
+            for (auto x = 0; x < this->_width; x++)
+                push(x, y);
     }
-    // Bottom
-    if (y < this->_height-1) {
-        auto bottom_node = _nodes[(y+1) * _width + x];
-        node->_capacities[2] = DISTANCE(node->_gray_val, bottom_node->_gray_val);
+}
+
+void Graph::push(int x, int y) {
+    if (!is_active(x, y))
+        return;
+    auto idx_curr = y * this->_width + x;
+
+    for (auto i = 0; i < 4; i++) {
+        auto idx_nghb = (y + this->y_nghb[i]) * _width + (x + this->x_nghb[i]);
+        if (y + y_nghb[i] < 0 || y + y_nghb[i] >= _height ||
+            x + x_nghb[i] < 0 || x + x_nghb[i] >= _width)
+            continue;
+        // Pushed flow
+        float flow = std::min(this->_neighbors[i][idx_curr], this->_excess_flow[idx_curr]);
+        // Update excess flow
+        this->_excess_flow[idx_curr] -= flow;
+        this->_excess_flow[idx_nghb] += flow;
+        // Update edge capcities
+        this->_neighbors[i][idx_curr] -= flow;
+        this->_neighbors[id_opp[i]][idx_nghb] += flow;
     }
-    // Left
-    if (x > 0) {
-        auto left_node = _nodes[y * _width + (x-1)];
-        node->_capacities[3] = DISTANCE(node->_gray_val, left_node->_gray_val);
-    }
+}
+
+void Graph::relabel(int x, int y) {
+
+}
+bool Graph::is_active(int x, int y) {
+    return true;
+
 }
--- cpu_implem/src/graph.hh
+++ cpu_implem/src/graph.hh
@@ -8,31 +8,36 @@
 #include <string>
 #include <vector>
 #include <memory>
-#include "node.hh"
-
-using shared_node = std::shared_ptr<Node>;
 
 class Graph {
 public:
     Graph(char *img, char *seeds);
+    void max_flow();
+    void push(int x, int y);
+    void relabel(int x, int y);
+    bool is_active(int x, int y);
 
 private:
     void normalize_histo(size_t *bck_histo, size_t *obj_histo, float **norm_bck_histo, float **norm_obj_histo, float *sum_bck_histo, float *sum_obj_histo);
-    void initialize_node_capacities(int x, int y, shared_node node);
+    void initialize_node_capacities(int x, int y, std::vector<int> &grays);
 
 
-    std::vector<shared_node> _nodes;
     int _width;
     int _height;
+    int _size;
+    unsigned int _nb_active;
 
-    /* NEXT STEP
-    float *excess_flow;
-    float *heights; x2
-    float *up;
-    float *right;
-    float *bottom;
-    float *left;
-    */
+    std::vector<int> _heights;
+    std::vector<float> _excess_flow;
+    std::vector<float> _up;
+    std::vector<float> _right;
+    std::vector<float> _bottom;
+    std::vector<float> _left;
+
+    std::vector<float> _neighbors[4];    // {up, right, bottom, left}
+    const int x_nghb[4] = {0, 1, 0, -1}; // idx offset for x axis
+    const int y_nghb[4] = {-1, 0, 1, 0}; // idx offset for y axis
+    const int id_opp[4] = {2, 3, 0, 1};  // idx of opposite array up=0, right=1, bottom=2, left=3
 
 };
 
