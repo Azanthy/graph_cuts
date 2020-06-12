cbb32e7e78e672ae5442f16dadc1da7d17345377
Alexandre Girard <alexandre.girard@epita.fr>
2020-06-12 23:07:05 +0200

    [ARCH] added options for cpu or gpu

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,40 @@
 #include <iostream>
-
+#include <unistd.h>
 #include "graph.hh"
 #include "gpu.hh"
 
+int parse_options(int argc, char *argv[])
+{
+    int opt;
+    while ((opt = getopt(argc, argv, ":cg")) != -1)
+    {
+        switch (opt)
+        {
+            case 'c':
+                return 0;
+            case 'g':
+                return 1;
+            default:
+                return -1;
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
-    if (argc != 3) {
-        std::cerr << "graph_cuts: should be ./graph_cut <img> <seeds_img>" << std::endl;
+    if (argc < 3) {
+        std::cerr << "graph_cuts: should be ./graph_cut [-cg]<img> <seeds_img>" << std::endl;
         exit(-1);
     }
-    auto graph = Graph(argv[1], argv[2], 1000);
-    max_flow_gpu(graph);
-    //graph.max_flow();
+    int algo = parse_options(argc, argv);
+    std::cout << algo << std::endl;
+    std::cout << optind<<" "<< argv[optind]<<" "<< argv[optind+1]<<std::endl;
+    auto graph = Graph(argv[optind], argv[optind+1], 1000);
+    if (!algo)
+        graph.max_flow();
+    else
+        max_flow_gpu(graph);
     graph.dfs();
     graph.print();
 }
