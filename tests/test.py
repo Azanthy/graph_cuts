1dc6ebd6c20fd51d4ab8db9b5f05cdc9199563c2
Mehdi <mehdi.bekhtaoui@epita.fr>
2020-06-13 17:50:33 +0200

    Test Suite

--- tests/test.py
+++ tests/test.py
@@ -1,31 +1,38 @@
 #! /usr/bin/env python3
 
-import cv2, sys, os
+import sys, os
+from skimage.io import imread
+from sklearn.metrics import f1_score
 import numpy as np
 
-def main(argv):
-    if len(argv) != 1:
-        return
-    binary = argv[0]
-    INPUT_PATH = '../segmentation_dataset/inputs/'
-    EXPECTED_PATH = '../segmentation_dataset/expected_outputs/'
-    imgs_normal = [ f for f in os.listdir(INPUT_PATH + 'normal/') ]
-    imgs_marked = [ "marked_" + f for f in imgs_normal ]
-    print([p[:-4] for p in imgs_normal])
-    for i in range(len(imgs_normal)):
-        line = binary + " -i " + INPUT_PATH+'normal/'+imgs_normal[i] + " " + \
-                                 INPUT_PATH+'marked/'+imgs_marked[i] + \
-                        " -o " + "out/"+imgs_normal[i]
-        print(line) #replace here by the execution of the command line
-        expected_name = imgs_normal[i]
-        if expected_name[-3:] == "jpg":
-            expected_name = expected_name[:-3] + "png"
-        expected = cv2.imread(os.path.abspath(EXPECTED_PATH+expected_name))
-        got = cv2.imread(os.path.abspath("out/"+expected_name))
-        diff = np.count_nonzero(cv2.bitwise_xor(expected, got))
-        width, height, _ = expected.shape
-        percentage = 100. * float(diff) / float(width * height)
-        print("Percentage of difference for "+imgs_normal[i]+ " is "+ str(percentage))
+PATH_TO_DATASET = "../segmentation_dataset/"
+BUILD_FOLDER = "../build/"
+NAME_OF_EXEC = "graph_cuts"
+NUMBER_OF_TESTS = 4 # -1 to test the whole dataset
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+if __name__ == '__main__':
+    files = os.listdir(os.path.join(PATH_TO_DATASET, "inputs", "normal"))
+    if NUMBER_OF_TESTS != -1:
+        files = files[:NUMBER_OF_TESTS]
+    exec_path = os.path.join(BUILD_FOLDER, NAME_OF_EXEC)
+    scores = []
+    for target_file in files:
+        print("=======================================")
+        print("Creating " + target_file)
+        os.system(exec_path + ' ' + 
+            os.path.join(PATH_TO_DATASET, "inputs", "normal", target_file) + ' ' +
+            os.path.join(PATH_TO_DATASET, "inputs", "marked", "marked_" + target_file))
+        if os.path.exists("output.jpg"):
+            print("Created " + target_file + " output")
+        else:
+            print("Unable to create " + target_file + " output")
+            continue
+        os.rename("output.jpg", "output_" + target_file)
+        output = imread("output_" + target_file, as_gray=True).astype('int') * 255
+        expected = imread(os.path.join(PATH_TO_DATASET, "expected_outputs", target_file), as_gray=True).astype('int')
+        print(output.max(), expected.max())
+        score = f1_score(expected.reshape(-1), output.reshape(-1), average='micro')
+        scores.append(score)
+        print("DICE_COEFFICIENT : " + str(score))
+    print("================")
+    print("Mean DICE " + str(np.array(scores).mean()) + " on " + str(len(scores)) + " tests.")
